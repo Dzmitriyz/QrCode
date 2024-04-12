d4ca47595f40c5ac090afe5430db9ca1f578d1ba
Dzmitriyz <hekto2004@mail.ru>
2024-04-13 03:52:10 +0700

    Added the ability to share QR Code

--- app/src/main/java/com/example/qrcode/MainActivity.kt
+++ app/src/main/java/com/example/qrcode/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.qrcode
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
@@ -13,28 +17,57 @@ import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class MainActivity : AppCompatActivity() {
     var im: ImageView? = null
-    var Bgenerate:Button? = null
+    var Bgenerate: Button? = null
+    var bShare: Button? = null
     var edT: EditText? = null
+    var bitmapMain: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+        Bgenerate?.setOnClickListener {
+            if (edT != null) {
+                generateQrCode(edT?.text.toString())
+                bShare?.visibility = View.VISIBLE
+            }else{
+                toastMessage()
+            }
+        }
+        bShare?.setOnClickListener {
+            val bitmpath = MediaStore.Images.Media.insertImage(contentResolver, bitmapMain, "upload", null)
+            val uri = Uri.parse(bitmpath)
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "img/png"
+                putExtra(Intent.EXTRA_STREAM, uri)
+            }
+            val shareIntent = Intent.createChooser(sendIntent,getString(R.string.share_qr))
+            startActivity(shareIntent)
+        }
+    }
+
+
+    private fun init(){
         im = findViewById(R.id.imageView)
         Bgenerate = findViewById(R.id.button)
         edT = findViewById(R.id.editTextText)
-
-        Bgenerate?.setOnClickListener {
-            generateQrCode("lol")
-        }
+        bShare = findViewById(R.id.share)
     }
-    private fun generateQrCode(text: String){
+
+    private fun generateQrCode(text: String) {
         try {
-        val barcodeEncode: BarcodeEncoder= BarcodeEncoder()
-        val bitmap: Bitmap = barcodeEncode.encodeBitmap(text,BarcodeFormat.QR_CODE, 700,700)
-        //val qrGenerator = QRGEncoder(text, null, QRGContents.Type.TEXT, 200)
-            //val bMap = qrGenerator.encodeAsBitmap()
+            val barcodeEncode: BarcodeEncoder = BarcodeEncoder()
+            val bitmap: Bitmap = barcodeEncode.encodeBitmap(text, BarcodeFormat.QR_CODE, 700, 700)
             im?.setImageBitmap(bitmap)
-        }catch (e: WriterException){
+            bitmapMain =bitmap
+        } catch (e: WriterException) {
 
         }
     }
+    private fun toastMessage() {
+        var toast = Toast.makeText(this, "Введите текст", Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+
 }
